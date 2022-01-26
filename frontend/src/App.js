e2870b88452d2c49639bd83e47e3156bb908201b
Amina Chaabane <amina.chaabane34@gmail.com>
2022-01-26 13:52:46 +0100

    add Admin home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import AdminHome from './pages/AdminHome';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/AdminHome' element={<AdminHome />} />
     </Routes>
     </Router>
 
---
+++ frontend/src/pages/AdminHome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuAdmin from '../components/MenuAdmin';
+import NavAdmin from '../components/NavAdmin';
+
+const AdminHome = () => {
+    return (
+        <div>
+            <NavAdmin />
+            <MenuAdmin />
+        </div>
+    );
+};
+
+export default AdminHome;
