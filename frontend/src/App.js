474a6886d230afba30f0be47049e6e0a82a04a36
Elkaouni <76531172+Elkaouni@users.noreply.github.com>
2022-01-26 17:18:21 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,17 @@ import Login from './pages/Login';
 import AdminHome from './pages/AdminHome';
 import UsersList from './pages/UsersList';
 
+import HomeUser from './pages/User/HomeUser';
+import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
+import AuctionInfo from './pages/User/AuctionInfo';
+import MyProfil from "./pages/User/MyProfil";
+import Results from "./pages/User/Results";
+import Notifications from "./pages/User/Notifications";
+import NewAuction from "./pages/User/NewAuction";
+import PricavyPolicy from "./pages/Footer/PricavyPolicy";
+
+
 function App() {
 
   return (
@@ -15,6 +26,21 @@ function App() {
         <Route exact path='/login' element={<Login/>} />
         <Route exact path='/AdminHome' element={<AdminHome />} />
         <Route exact path='/UsersList' element={<UsersList/>} />
+        
+          /*Pages User can access) */
+        <Route exact path="/sign-up" element={<SignUp/>} />
+
+        <Route exact path="/" element={<HomeUser/>} />
+        <Route exact path="/my-profil" element={<MyProfil/>} />
+        <Route exact path="/notifications" element={<Notifications/>} />
+        <Route path="/all-auctions" element={<Results/>} />
+        <Route path="/search/*" element={<Results/>} />
+        <Route exact path="/all-ended-auctions" element={<Results/>} />
+        <Route exact path="/auction-info" element={<AuctionInfo/>} />
+        <Route exact path="/new-auction" element={<NewAuction/>} />
+
+        <Route exact path="/privacy-policy" element={<PricavyPolicy />}/>
+        <Route path="*" element={<NotFound/>} />
     </Routes>
     </Router>
 
@@ -22,3 +48,4 @@ function App() {
 }
 
 export default App;
+
