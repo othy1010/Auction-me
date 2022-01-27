ec64c37f4a992d6e1843fc85f1e35226b8df9cde
Elkaouni <76531172+Elkaouni@users.noreply.github.com>
2022-01-28 00:05:00 +0100

    Update NewAuction.js

--- front-end/src/pages/User/NewAuction.js
+++ front-end/src/pages/User/NewAuction.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavUser from "../../components/User/NavUser"
 import UserFooter from "../../components/User/UserFooter";
+import NewAuctionForm from "../../components/User/NewAuctionForm";
 
 const NewAuction = () => {
     return (
         <div>
             <NavUser />
-            NewAuction form
+            <br/>
+            <NewAuctionForm/>
             <UserFooter />
         </div>
     );
