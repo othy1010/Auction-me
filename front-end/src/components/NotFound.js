5e88220303f2ef96905a68d87ecd221cd86472a6
Elkaouni <76531172+Elkaouni@users.noreply.github.com>
2022-01-28 20:21:04 +0100

    Update NotFound.js

--- front-end/src/components/NotFound.js
+++ front-end/src/components/NotFound.js
@@ -3,6 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
+            <br/>
             <h1> Erreur 404</h1>
             The page you are looking for does not exist.
         </div>
