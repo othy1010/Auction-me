3398cb9d31170237ee388d720b0e88609e57b13f
Elkaouni <76531172+Elkaouni@users.noreply.github.com>
2022-02-25 22:41:12 +0100

    Update Login.js

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,26 +1,140 @@
 import React from 'react';
-import '../styles/admin_styles/Login.css'
-import {Link} from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {Container} from "@material-ui/core";
 
-const Login = () => {
-    return (
-        <div className="center">
-            <h1>Login</h1>
-            <form>
-                <div className="txt_field">
-                    <input type="email" placeholder='Email' required/>
-                </div>
-                <div className="txt_field">
-                    <input type="password" placeholder='Password' required/>
-                </div>
-                <div className="pass">Forgot Password?</div>
-                <input type="submit" value="Login" />
-                <div className="signup_link">
-                    Not a member? <Link to="/sign-up">Signup</Link>
-                </div>
-            </form>
-        </div>
-    );
+import '../styles/admin_styles/Login.css'
+import {Link, useParams, useLocation, BrowserRouter} from "react-router-dom";
+import {Component} from "react";
+import { Router, Switch as Routes, Route} from 'react-router-dom';
+import toast from "react-hot-toast";
+import ReactDOM from "react-dom";
+import UserService from "../Service/UserService";
+import "../configs/UserInfo";
+import UserInfo from "../configs/UserInfo";
+import AdminHome from "./Admin/AdminHome";
+import HomeUser from "./User/HomeUser";
+
+
+
+// eslint-disable-next-line no-undef
+/*
+function loginParam(props){
+    const {newUser} = useParams();
+    const {stateParams} = useLocation().state;
+    console.log("stateParams : "+ stateParams)
+}    */
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        };
+        this.changeEmailHandler = this.changeEmailHandler.bind(this);
+        this.changePasswordHandler = this.changePasswordHandler.bind(this);
+    }
+    changeEmailHandler = (event) => {
+        this.setState({ email: event.target.value });
+    };
+
+    changePasswordHandler = (event) => {
+        this.setState({ password: event.target.value });
+    };
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    };
+    connect = (e) => {
+        e.preventDefault();
+        var html = "<div class='alert alert-danger'>";
+        // eslint-disable-next-line no-undef
+        UserService.findByEmail(this.state.email).then((response) => {
+            if (response.data == "") {
+                html += "<div>Cet email n'existe pas</div>";
+                html += "</div>";
+                document.getElementById("errorsLogin").innerHTML = html;
+                toast.error("ERREUR");
+            }
+            else {
+                if (response.data.password != this.state.password) {
+                    html += "<div>Fausse combinaison</div>";
+                    html += "</div>";
+                    document.getElementById("errorsLogin").innerHTML = html;
+                    toast.error("ERREUR");
+
+                } else {
+                    // eslint-disable-next-line no-undef
+                    UserInfo.userInfos = response.data;
+                    localStorage.setItem(
+                        "userInfos",
+                        // eslint-disable-next-line no-undef
+                        JSON.stringify(UserInfo.userInfos)
+                    );
+                    if(response.data.isAdmin){
+                        //this.props.history.push("/admin-profil");
+                        toast.success("Connection avec succés");
+                        ReactDOM.render(
+                            <React.StrictMode>
+                                <Router>
+                                    <AdminHome />
+                                </Router>
+                            </React.StrictMode>,
+                            document.getElementById("root")
+                        );
+                    }
+                    else {
+                        this.props.history.push("/my-profil")
+
+                        toast.success("Connection avec succés");
+                        ReactDOM.render(
+                            <React.StrictMode>
+                            <Router>
+                                <HomeUser />
+                            </Router>
+                        </React.StrictMode>,
+                            document.getElementById("root")
+                        );
+                    }
+                }
+            }
+        });
+    };
+
+
+
+    render() {
+
+
+        return (
+
+            <Container>
+            <div className="center">
+                <h1>Login</h1>
+                <Form>
+                    <div className="txt_field">
+                        <input name="field" type="email" placeholder='Email' required
+                               value={this.state.email}
+                               onChange={this.changeEmailHandler}/>
+                    </div>
+                    <div className="txt_field">
+                        <input type="password" placeholder='Password' required
+                               value={this.state.password} name="field"
+                               onChange={this.changePasswordHandler}/>
+                    </div>
+                    <div className="pass">Forgot Password?</div>
+                    <Button id="submit"
+                            size="lg"
+                            onClick={this.connect}
+                            disabled={!this.validateForm()}>Login</Button>
+                    <div className="signup_link">
+                        Not a member? <Link to="/sign-up">Signup</Link>
+                    </div>
+                </Form>
+                <div id="errorsLogin"></div>
+            </div>
+            </Container>
+        );
+    }
 };
 
 export default Login;
